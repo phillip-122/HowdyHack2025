8fcd5ba1b43fcf2b0d9ac5baf30906014089d773
phillip-122 <goodlavaboy@gmail.com>
2025-10-19 01:30:43 -0500

    Model training file

--- trainModel.py
+++ trainModel.py
@@ -1,14 +1,17 @@
 from ultralytics import YOLO
 
-model = YOLO("yolo11l-pose.pt")  # load an official model
-# model = YOLO("path/to/best.pt")  # load a custom model
+def main(): 
+    model = YOLO("yolov8l.pt")
 
-# Predict with the model
-results = model.predict(r"C:\Users\Phillip\Desktop\coding projects\HowdyHack2025\HowdyHack2025\Tricks_Ollie_Ollie0.mov", show=True, save=True)  # predict on an image
-#model.predict or model.track
+    results = model.train(
+        data=r"wheelsTraining\data.yaml",
+        epochs=50,
+        imgsz=640,
+        batch=16,
+        project="runs/detect/skateboards_train",
+        name="train1",
+        exist_ok=True
+    )
 
-# Access the results
-for result in results:
-    xy = result.keypoints.xy  # x and y coordinates
-    xyn = result.keypoints.xyn  # normalized
-    kpts = result.keypoints.data  # x, y, visibility (if available)
+if __name__ == "__main__":
+    main()
